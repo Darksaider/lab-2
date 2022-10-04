606f9711a2ef9bf0472a3275613acb00e0a7058d
Darksaider <45597235+Darksaider@users.noreply.github.com>
2022-10-04 00:15:31 +0000

    Update 2_lab_2.c

--- 2_lab_2.c
+++ 2_lab_2.c
@@ -5,16 +5,7 @@ int main(){
     c = -2; 
 
     float y = 4 - ((3 * ( a + b + c)) / (2 - c)) - (1 / b);
-    float one= (3 * ( a + b + c));
-    float two= 4 - (3 * ( a + b + c) / (2 -c)) - (1/b);
-    float three = 2 -c;
-    float four = one / 4;
-    float five = 1 / b;
-    
+
     printf("Результат y = %g \n", y );
-    printf("Результат y = %g \n", one );
-    printf("Результат y = %g \n", two );
-    printf("Результат y = %g \n", three );
-    printf("Результат y = %g \n", four );
-    printf("Результат y = %g \n", five );
+  
 }
